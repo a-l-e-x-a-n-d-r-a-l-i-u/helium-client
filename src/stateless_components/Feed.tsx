9a5c00513517413bbc0f71bd87d55243ee0aa4b9
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-08 18:20:45 +1100

    Added responsive sizing to <article> card header and footer

--- src/stateless_components/Feed.tsx
+++ src/stateless_components/Feed.tsx
@@ -13,20 +13,23 @@ const PostList: React.FC = () => {
       {postlist.map((post, i) => {
         return (
           <article>
-            {/* className='meteredContent' */}
             {/* could use next.js for responsive images, if can be bothered learning something new */}
             {/* should use a compressed version of images for a profile icon as well, otherwise it will take forever to load on a speedtest */}
-            <div className='card-header'>
-              <img className='profile-thumbnail' src={profilePlaceholder} alt={post.author} />
-              {/* actually the src should be more like {process.env.PUBLIC_URL + image} except you haven't set up process.env yet */}
-              {/* include post.org if it's written by a person or by an org. but also tweak getposts.js to hide if one or the other doesn't exist, we don't need a billion cards with 'Unknown author in Save the Children', just say 'Save the Children' */}
-              <div className='author-date-container'>
-                <span>{post.author}</span>
-                <span>·</span>
-                <span>{post.date}</span>
+            <div className='wrapper'>
+              <div className='card-header'>
+                <img className='profile-thumbnail' src={profilePlaceholder} alt={post.author} />
+                {/* actually the src should be more like {process.env.PUBLIC_URL + image} except you haven't set up process.env yet */}
+                {/* include post.org if it's written by a person or by an org. but also tweak getposts.js to hide if one or the other doesn't exist, we don't need a billion cards with 'Unknown author in Save the Children', just say 'Save the Children' */}
+                <div className='author-date-container'>
+                  <span>{post.author}</span>
+                  <span>·</span>
+                  <span>{post.date}</span>
+                </div>
               </div>
+              <div className='thumbnail-width'></div>
             </div>
-            <div className='post-summary'>
+
+            <div className='card-body'>
               <div className='blurb-container'>
                 <h2>{post.title}</h2>
                 <p className='blurb'>{post.content}</p>
@@ -34,14 +37,17 @@ const PostList: React.FC = () => {
               </div>
               <img className='post-thumbnail xs-only' src={postPlaceholder} alt={post.title} />
             </div>
-            <div className='card-footer'>
-              <a href='/'>
-                {" "}
+
+            <div className='wrapper'>
+              <div className='card-footer'>
+                <a href='/'>
+                  {/* Dynamically generate tag links */}
+                  <div className='tag'>{capitalizeFirstLetter(post.tag)}</div>
+                </a>
                 {/* <div>Based on your reading history/Selected for you</div> */}
-                <div className='tag'>{capitalizeFirstLetter(post.tag)}</div>
-              </a>
-              {/* <div>Based on your reading history/Selected for you</div> */}
-              <div>Icons for Add / Minus</div>
+                <div>Icons for Add / Minus</div>
+                <div className='thumbnail-width'></div>
+              </div>
             </div>
           </article>
         );
