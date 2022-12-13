bf9742e67c17f5deb8ec456ee3c3de109196ec97
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-13 15:24:14 +1100

    Brushing up navigation links for top and bottom navbars

--- src/stateless_components/navigation/TopNav.tsx
+++ src/stateless_components/navigation/TopNav.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import placeholderReactLogo from "../../images/logo192.png";
+import lists_link from "../../images/lists_link.svg";
 
 const TopNav = () => {
   return (
     <nav className='top-nav'>
       <img className='nav-logo' src={placeholderReactLogo} alt='placeholder logo' />
+
       <div className='nav-list'>
-        <li className='nav-option'>CTA button</li>
-        <li className='nav-option'>notifs to right</li>
+        <Link to='/login'>CTA Button</Link>
+        <Link to='/me/notifications'>
+          <img src={lists_link} alt='Notifications' />
+        </Link>
       </div>
     </nav>
   );
