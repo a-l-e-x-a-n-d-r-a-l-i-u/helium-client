9a84702af7f7df413fecac82a2492e7f77faa65d
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-13 11:46:37 +1100

    Building out navigation with SideNav component

--- src/stateless_components/navigation/SideNav.tsx
+++ src/stateless_components/navigation/SideNav.tsx
@@ -1,27 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import placeholderReactLogo from "../../images/logo192.png";
+
+import home_link from "../../images/home_link.svg";
+import home_active from "../../images/home_active.svg";
+import lists_link from "../../images/lists_link.svg";
+import lists_active from "../../images/lists_active.svg";
+import search_link from "../../images/search_link.svg";
+import search_active from "../../images/search_active.svg";
 
 const SideNav = () => {
   return (
     <nav className='side-nav'>
-      <img
-        className='nav-logo'
-        src={placeholderReactLogo}
-        alt='placeholder logo'
-      />
-      <ul className='nav-list'>
-        <li className='nav-option'>Home</li>
-        <li className='nav-option'>Notifs</li>
-        <li className='nav-option'>Lists</li>
-        <li className='nav-option'>Posts</li>
+      <img className='nav-logo' src={placeholderReactLogo} alt='placeholder logo' />
+      <div className='nav-list'>
+        <Link to='/'>
+          <img src={home_link} alt='Home' />
+        </Link>
+        <Link to='/me/notifications'>
+          <img src={lists_link} alt='Notifications' />
+        </Link>
+        <Link to='/me/lists'>
+          <img src={lists_link} alt='Lists' />
+        </Link>
+        <Link to='/me/posts/drafts'>
+          <img src={lists_link} alt='My Posts' />
+        </Link>
         <br />
-        <li className='nav-option'>Write</li>
-      </ul>
-      <img
-        className='profile-thumbnail'
-        src={placeholderReactLogo}
-        alt='placeholder thumbnail'
-      />
+        <Link to='/me/write'>
+          <img src={lists_link} alt='Write' />
+        </Link>
+      </div>
+      <img className='profile-thumbnail' src={placeholderReactLogo} alt='placeholder thumbnail' />
     </nav>
   );
 };
