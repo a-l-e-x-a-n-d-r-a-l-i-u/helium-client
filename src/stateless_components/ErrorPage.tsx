b06aefb355940f7607b892fde245462c1f681666
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-16 13:55:28 +1100

    added navigate object to useNavigate in ErrorPage.tsx

--- src/stateless_components/ErrorPage.tsx
+++ src/stateless_components/ErrorPage.tsx
@@ -1,15 +1,27 @@
-import { useRouteError } from "react-router-dom";
+import { useEffect } from "react";
+import { useRouteError, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
+  const navigate = useNavigate();
+  // You can pass -1 into navigate(-1) to go back 1 page (like clicking 'back')
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  });
+
   return (
-    <div id='error-page'>
+    <>
+      {/* <div id='error-page'> */}
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
-    </div>
+      {/* <p><i>{error.statusText || error.message}</i></p> */}
+      {/* </div> */}
+    </>
   );
 };
 
--- src/stateless_components/Feed.tsx
+++ src/stateless_components/Feed.tsx
@@ -14,13 +14,11 @@ const PostList: React.FC = () => {
       {postlist.map((post, i) => {
         return (
           <article>
-            {/* could use next.js for responsive images, if can be bothered learning something new */}
-            {/* should use a compressed version of images for a profile icon as well, otherwise it will take forever to load on a speedtest */}
+            {/* should use a compressed version of images, otherwise it will take forever to load on a speedtest */}
             <div className='wrapper'>
               <div className='card-header'>
                 <img className='profile-thumbnail' src={profilePlaceholder} alt={post.author} />
                 {/* actually the src should be more like {process.env.PUBLIC_URL + image} except you haven't set up process.env yet */}
-                {/* include post.org if it's written by a person or by an org. but also tweak getposts.js to hide if one or the other doesn't exist, we don't need a billion cards with 'Unknown author in Save the Children', just say 'Save the Children' */}
                 <div className='author-date-container'>
                   <span>{post.author}</span>
                   <span>·</span>
@@ -36,8 +34,7 @@ const PostList: React.FC = () => {
                   <h2>{post.title}</h2>
                 </Link>
                 {/* style link text black */}
-                <p className='blurb'>{post.content}</p>
-                {/* figure out how to shorten post.content into max 3lines of text */}
+                <p className='blurb'>{post.content.split(".")}</p>
               </div>
               <img className='post-thumbnail xs-only' src={postPlaceholder} alt={post.title} />
             </div>
@@ -48,8 +45,9 @@ const PostList: React.FC = () => {
                   {/* Dynamically generate tag links */}
                   <div className='tag'>{capitalizeFirstLetter(post.tag)}</div>
                 </Link>
+                {/* Scoped out the following parts of Medium.com for this project */}
                 {/* <div>Based on your reading history/Selected for you</div> */}
-                <div>Icons for Add / Minus</div>
+                {/* <div>Icons for Add / Minus</div> */}
               </div>
               <div className='thumbnail-width'></div>
             </div>
@@ -62,8 +60,6 @@ const PostList: React.FC = () => {
 
 const FeedNav: React.FC = () => {
   return (
-    // how to programmatically show the 'selected' state ???????? use for loop to loop thru list of tabs ????
-
     <div id='feed-nav'>
       <NavLink to='/recommendations'>
         <div className='tab symbol'>+</div>
