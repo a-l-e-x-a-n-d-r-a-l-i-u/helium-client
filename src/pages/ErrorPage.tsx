5b5111b6226fae11e5cc7eb28961de9de69855ab
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-23 11:36:33 +1100

    Update ErrorPage.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { useRouteError, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  //  const error = useRouteError();
-  //  console.error(error);
+  const error = useRouteError();
+  console.error(error);
 
   const navigate = useNavigate();
   // navigate(-1) will take you back 1 page (like clicking 'back')
@@ -18,7 +18,9 @@ const ErrorPage = () => {
     <div id='error-page'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
     </div>
   );
 };
