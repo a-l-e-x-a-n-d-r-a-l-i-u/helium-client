39856200a617b56a64c6b1ce37e90baa102eff15
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-30 09:51:51 +1100

    Revert "I guess you *could* put state in a higher order component but ehhhh you'll just be passing props everywhere"
    This reverts commit 22cbe8baedcd8322abeed6628c28546c74145683.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import { useContext } from "react";
-
-// If I wanted state in a Higher Order Component
-import { useState } from "react";
-
 import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
 import "./assets/styling/styles.scss";
 //import { UserContext } from "../containers/UserContext";
@@ -62,9 +58,6 @@ Links I copied from Medium.com:
 */
 
 const App = () => {
-  const [user, setUser] = useState<LoginProps | null>(null);
-  const [showProfile, setShowProfile] = useState(false);
-
   return <RouterProvider router={router} />;
 };
 
