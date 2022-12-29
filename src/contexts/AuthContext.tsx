676a36f923edcf4f527afae2582466dc5354c9e5
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-30 10:57:21 +1100

    AuthContext.tsx is buggy fml

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,15 @@
+<<<<<<< Updated upstream
 import { createContext, useState } from "react";
+=======
+import { createContext, ReactNode, useState } from "react";
+>>>>>>> Stashed changes
 
 // Difference between type and interface is interfaces are an extension
 // Oh wait actually there's more to it than that
 // https://medium.com/@martin_hotell/interface-vs-type-alias-in-typescript-2-7-2a8f1777af4c
 
 type authContextType = {
+<<<<<<< Updated upstream
   isLoggedIn: boolean;
   login: () => void;
   logout: () => void;
@@ -35,4 +40,34 @@ export const AuthProvider: React.FC = ({ children }) => {
   };
 
   return <AuthContext.Provider value={(isLoggedIn, setLoggedIn, username)}>{children}</AuthContext.Provider>;
+=======
+  user: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+// AuthContext
+const authContextDefaultValues: authContextType = {
+  user: false,
+  login: () => {},
+  logout: () => {},
+};
+
+export const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+type authProviderProps = {
+  children: ReactNode;
+};
+// AuthProvider function
+export const AuthProvider = ({ children }: authProviderProps) => {
+  const [user, setUser] = useState<boolean>(false);
+  const login = () => {
+    setUser(true);
+  };
+  const logout = () => {
+    setUser(false);
+  };
+
+  return <AuthContext.Provider value={(user, login, logout)}>{children}</AuthContext.Provider>;
+>>>>>>> Stashed changes
 };
