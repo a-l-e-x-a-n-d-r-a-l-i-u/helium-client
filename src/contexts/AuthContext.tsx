6917ec2a1edb9c92847ae99fac4b6cc48e7bbd8f
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-30 09:43:32 +1100

    ARRGGH Typescript throwing errors!!!!

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,38 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 // Difference between type and interface is interfaces are an extension
 // Oh wait actually there's more to it than that
 // https://medium.com/@martin_hotell/interface-vs-type-alias-in-typescript-2-7-2a8f1777af4c
 
 type authContextType = {
-  isLoggedIn: boolean;
+  user: boolean;
   login: () => void;
   logout: () => void;
-  username?: string;
-  password?: string;
 };
 
 // AuthContext
 const authContextDefaultValues: authContextType = {
-  isLoggedIn: false,
+  user: false,
   login: () => {},
   logout: () => {},
-  username: undefined,
-  password: undefined,
 };
 
 export const AuthContext = createContext<authContextType>(authContextDefaultValues);
 
+type authProviderProps = {
+  children: ReactNode;
+};
 // AuthProvider function
-export const AuthProvider: React.FC = ({ children }) => {
-  const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+export const AuthProvider = ({ children }: authProviderProps) => {
+  const [user, setUser] = useState<boolean>(null);
   const login = () => {
-    setLoggedIn(true);
+    setUser(true);
   };
-
   const logout = () => {
-    setLoggedIn(false);
+    setUser(false);
   };
 
-  return <AuthContext.Provider value={(isLoggedIn, setLoggedIn, username)}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={(user, login, logout)}>{children}</AuthContext.Provider>;
 };
