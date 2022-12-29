2d2942e86121441feb85628ad359f1d20328504f
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-30 09:51:43 +1100

    Revert "Ugh Context API is hard"
    This reverts commit 45e696df616691c362913915f9cc2cac940f56b6.

--- src/pages/LoginModal.tsx
+++ src/pages/LoginModal.tsx
@@ -1,10 +1,15 @@
-// State
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-
+import { useState } from "react";
 // import from the Modal.tsx component later
+
+interface LoginProps {
+  username?: string;
+  password?: string;
+  prevState?: null;
+}
+
 const LoginModal = () => {
-  //  const { user, setUser } = useContext(AuthContext);
+  const [user, setUser] = useState<LoginProps | null>(null);
+  const [showProfile, setShowProfile] = useState(false);
   return (
     <div>
       Log in here
@@ -13,20 +18,26 @@ const LoginModal = () => {
         id='username'
         type='text'
         placeholder='Enter username'
-        // onChange={(e) => {
-        //   setUser({ username: e.target.value });
-        // }}
+        onChange={(e) => {
+          setUser({ username: e.target.value });
+        }}
       />
       <label htmlFor='password'>Password</label>
       <input
         id='password'
         type='text'
         placeholder='Enter password'
-        // onChange={(e) => {
-        //   setUser({ password: e.target.value });
-        // }}
+        onChange={(e) => {
+          setUser({ password: e.target.value });
+        }}
       />
-      <button>Sign In</button>
+      <button
+        onClick={() => {
+          setShowProfile(true);
+        }}>
+        Sign In
+      </button>
+      {showProfile && user && <h1>{user.username}</h1>}
     </div>
   );
 };
